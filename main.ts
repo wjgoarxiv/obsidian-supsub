b17d94b3985895f0ae2faa7d6befa98e39fb37c5
wjgoarxiv <118244317+wjgoarxiv@users.noreply.github.com>
2023-07-26 12:25:48 +0900

    Update `main.ts`

--- main.ts
+++ main.ts
@@ -1,21 +1,32 @@
-import { Plugin } from "obsidian";
+import { Plugin, MarkdownView, Editor } from "obsidian";
 
 export default class SupSubPlugin extends Plugin {
 	onload() {
 		this.addCommand({
 			id: "wrap-sup",
 			name: "Wrap with <sup> tags",
-			callback: () => this.wrapSelection("sup"),
+			editorCallback: (editor, view) => this.wrapSelection("sup", editor),
+			hotkeys: [
+				{
+					modifiers: ["Mod", "Alt"],
+					key: "=",
+				},
+			],
 		});
 		this.addCommand({
 			id: "wrap-sub",
 			name: "Wrap with <sub> tags",
-			callback: () => this.wrapSelection("sub"),
+			editorCallback: (editor, view) => this.wrapSelection("sub", editor),
+			hotkeys: [
+				{
+					modifiers: ["Mod", "Alt"],
+					key: "-",
+				},
+			],
 		});
 	}
 
-	wrapSelection(tag: string) {
-		const editor = this.app.workspace.activeLeaf.view.sourceMode.cmEditor;
+	wrapSelection(tag: string, editor: Editor) {
 		const selection = editor.getSelection();
 
 		const regex = new RegExp(`<${tag}>(.*?)<\/${tag}>`);
